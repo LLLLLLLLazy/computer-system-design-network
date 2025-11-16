571b2a9efd73c910692f82fec0286c927dfa428e
Lazy <lazy@LazydeMacBook-Air.local>
2025-11-16 15:28:33 +0800

    first commit

---
+++ project/target/debug/deps/project-2b2661865ca160ab.d
@@ -0,0 +1,5 @@
+/Users/lazy/code/network/project/target/debug/deps/project-2b2661865ca160ab.d: src/main.rs
+
+/Users/lazy/code/network/project/target/debug/deps/libproject-2b2661865ca160ab.rmeta: src/main.rs
+
+src/main.rs:
---
+++ project/target/debug/deps/project-c7697c7f2889a391.d
@@ -0,0 +1,5 @@
+/Users/lazy/code/network/project/target/debug/deps/project-c7697c7f2889a391.d: src/main.rs
+
+/Users/lazy/code/network/project/target/debug/deps/libproject-c7697c7f2889a391.rmeta: src/main.rs
+
+src/main.rs:
