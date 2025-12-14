7cd67739755cb680b44dba3c75820ac42558ecb3
Lazy <lazy@LazydeMacBook-Air.local>
2025-12-14 16:48:22 +0800

    回退

--- net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305.d
+++ net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305.d
@@ -1,5 +1,5 @@
-/Users/lazy/code/network/net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305.d: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.30.1/build.rs
+/Users/lazy/code/network/computer-system-design-network/net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305.d: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.30.1/build.rs
 
-/Users/lazy/code/network/net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.30.1/build.rs
+/Users/lazy/code/network/computer-system-design-network/net/target/debug/build/nix-5657e8383c0ea305/build_script_build-5657e8383c0ea305: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.30.1/build.rs
 
 /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.30.1/build.rs:
--- net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60.d
+++ net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60.d
@@ -1,5 +1,5 @@
-/Users/lazy/code/network/net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60.d: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pcap-2.3.0/build.rs
+/Users/lazy/code/network/computer-system-design-network/net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60.d: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pcap-2.3.0/build.rs
 
-/Users/lazy/code/network/net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pcap-2.3.0/build.rs
+/Users/lazy/code/network/computer-system-design-network/net/target/debug/build/pcap-6c141ca7c9381a60/build_script_build-6c141ca7c9381a60: /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pcap-2.3.0/build.rs
 
 /Users/lazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pcap-2.3.0/build.rs:
